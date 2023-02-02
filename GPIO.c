ad51d894a1f52babca5ac01ad147919b77ea9251
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-02-02 22:58:55 +0600

    White LED set

--- GPIO.c
+++ GPIO.c
@@ -13,10 +13,11 @@ void GPIO_Init(GPIO_TypeDef* GPIOx,GPIO_InitTypeDef *GPIO_Init)
 // You need to implement this function	
 
 		RCC->AHB1ENR |= (1<<0);
+		RCC->AHB1ENR |= (1<<1);
 	
 		int i=0;
 	
-		for (i=0; i<10; i++)
+		for (i=0; i<16; i++)
 		{
 			GPIOx->MODER |= (1<<2*i);
 		}
--- main.c
+++ main.c
@@ -4,6 +4,27 @@
 #include "SYS_INIT.h"
 #include "USART.h"
 
+typedef struct
+{
+    uint32_t green;
+    uint32_t yellow;
+    uint32_t red;
+} traffic;
+typedef struct
+{
+    traffic mode;
+    uint32_t top;
+    uint32_t bottom;
+    uint32_t light;
+} lane;
+
+traffic normal = {15000, 5000, 20000};
+traffic delayed = {25000, 5000, 30000};
+
+uint32_t trafficGenerator(void)
+{
+    return rand() % 3 + 1;
+}
 int main (void)
 {
 	initClock();
@@ -12,20 +33,28 @@ int main (void)
 	GPIO_InitTypeDef x;
 	
 	GPIO_Init(GPIOA, &x);
-	
-	uint32_t start=getmsTick();
+	GPIO_Init(GPIOB, &x);
 	
 	while(1)
 	{
-		/*
-		GPIO_WritePin(GPIOA, 0, GPIO_PIN_RESET);g1
-		GPIO_WritePin(GPIOA, 1, GPIO_PIN_RESET);y1	
-		GPIO_WritePin(GPIOA, 7, GPIO_PIN_RESET);r1
-		GPIO_WritePin(GPIOA, 8, GPIO_PIN_RESET);r2			
-		GPIO_WritePin(GPIOA, 4, GPIO_PIN_RESET);y2
-		GPIO_WritePin(GPIOA, 5, GPIO_PIN_RESET);g2	
-		*/
+/*
+		GPIO_WritePin(GPIOA, 0, GPIO_PIN_RESET);//g1
+		GPIO_WritePin(GPIOA, 1, GPIO_PIN_RESET);//y1	
+		GPIO_WritePin(GPIOA, 7, GPIO_PIN_RESET);//r1
+		GPIO_WritePin(GPIOA, 8, GPIO_PIN_RESET);//r2			
+		GPIO_WritePin(GPIOA, 4, GPIO_PIN_RESET);//y2
+		GPIO_WritePin(GPIOA, 5, GPIO_PIN_RESET);//g2		
+*/	
+
+		GPIO_WritePin(GPIOA, 6, GPIO_PIN_RESET);//g1
+		GPIO_WritePin(GPIOA, 9, GPIO_PIN_RESET);//y1	
+		GPIO_WritePin(GPIOA, 10, GPIO_PIN_RESET);//r1
+		GPIO_WritePin(GPIOA, 11, GPIO_PIN_RESET);//r2			
+		GPIO_WritePin(GPIOA, 12, GPIO_PIN_RESET);//y2
+		GPIO_WritePin(GPIOB, 5, GPIO_PIN_RESET);//g2	
+		GPIO_WritePin(GPIOB, 10, GPIO_PIN_RESET);//g2	
 		
+				
 		GPIO_WritePin(GPIOA, 0, GPIO_PIN_RESET);//start g1
 		GPIO_WritePin(GPIOA, 8, GPIO_PIN_RESET);//start r2		
 		ms_delay(3000);
